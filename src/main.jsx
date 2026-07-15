85733fc0bfef55778964303bcbf3a4b5c6b7cbaf
julian <julian.lettieri@gmail.com>
2026-07-15 10:35:01 -0300

    Add Microsoft Clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Clarity from '@microsoft/clarity'
+
 import './index.css'
 import App from './App.jsx'
+
+Clarity.init('xm4vumutla')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
